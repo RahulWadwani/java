50ba5f272e7c0e529359687597d306a2640f618e
RahulWadwani <rahulwadwani786@gmail.com>
2023-06-08 18:57:06 +0530

    Beginners java

---
+++ fundamentals of Java/first_fund.java
@@ -0,0 +1,8 @@
+public class first_fund{
+    public static void main(String[] args){
+    
+    System.out.println("Hello!");
+    }
+    
+    }
+    
---
+++ qspiders/pre-programming/practise1.java
@@ -0,0 +1,113 @@
+// write a java program to print 90 by using 1st method
+
+/*
+class Pract1
+{
+    public static void main(String[] args)
+    {
+        System.out.print(90);
+    }
+}
+*/
+
+// write a java program to print 180 by using 2nd method
+/*
+class Pract2
+{
+    public static void main(String[] args)
+    {
+        System.out.println(180);
+    }
+}
+*/
+
+
+// write a java program to print 45,90 by using 1st method
+/*
+class Pract3
+{
+    public static void main(String[] args)
+    {
+        System.out.print(45);
+        System.out.print(90);
+    }
+}
+*/
+
+// write a java program to print 75,77,89 by using 2nd method
+/*
+class Pract4
+{
+    public static void main(String[] args)
+    {
+        System.out.println(75);
+        System.out.println(77);
+        System.out.println(89);
+    }
+}
+*/
+
+// write a java program to print 51 by using 2nd method
+/*
+class Pract5
+{
+    public static void main(String[] args)
+    {
+        System.out.println(51);
+    }
+}
+*/
+
+// perimeter of a circle 
+/*
+class Peri_circle
+{
+    public static void main(String[] args)
+    {
+        int r = 7;
+        double p = 3.14;
+        System.out.println('Perimeter of the circle is :' + 2*p*r);
+    }
+}
+ */
+
+// Area of a circle
+/*
+class Area_circle
+{
+    public static void main(String[] args)
+    {
+        int r = 7;
+        double p = 3.14;
+        System.out.println('Area of circle is : '+p*r*r);
+    }
+}
+*/
+
+// perimeter of rectangle
+/*
+class Peri_rect
+{
+    public static void main(String[] args)
+    {
+        int l = 45;
+        int b = 20;
+        System.out.println("Perimeter of rectangle is: " + 2*(l+b));
+    }
+}
+*/
+
+
+// area of rectangle 
+/*
+class Area_rect
+{
+    public static void main(String[] args)
+    {
+        int l = 20;
+        int b = 10;
+        System.out.println('Area of rectangle is : ' + (l*b));
+    }
+}
+*/
+
